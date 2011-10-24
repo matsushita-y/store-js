811212cd25ec26abb0c4458339b068e86cf3670e
Frank Kohlhepp <frank@kohlhepp.me>
2011-10-24 16:30:28 +0200

    Added event support

--- source/store.js
+++ source/store.js
@@ -4,8 +4,12 @@
 // License: MIT-license
 //
 (function () {
-    var Store = this.Store = function (name, defaults) {
+    var Store = this.Store = function (name, defaults, watcherSpeed) {
+        var that = this;
         this.name = name;
+        this.bindings = {};
+        
+        // Set defaults
         if (defaults) {
             for (var key in defaults) {
                 if (defaults.hasOwnProperty(key) && this.get(key) === undefined) {
@@ -13,6 +17,38 @@
                 }
             }
         }
+        
+        // Faked Events
+        var fireEvent = function (name, value) {
+            ([name, "*"]).each(function (selector) {
+                if (that.bindings[selector]) {
+                    that.bindings[selector].each(function (callback) {
+                        callback(value, name, that.name);
+                    });
+                }
+            });
+        };
+        
+        var oldObj = this.toObject();
+        (function watcher() {
+            var newObj = that.toObject();
+            
+            for (var key in newObj) {
+                if (newObj.hasOwnProperty(key) && newObj[key] !== oldObj[key]) {
+                    fireEvent(key, newObj[key]);
+                    oldObj = newObj;
+                }
+            }
+            
+            for (var key in oldObj) {
+                if (oldObj.hasOwnProperty(key) && !newObj.hasOwnProperty(key)) {
+                    fireEvent(key, newObj[key]);
+                    oldObj = newObj;
+                }
+            }
+            
+            setTimeout(watcher, (watcherSpeed || 300));
+        }());
     };
     
     Store.clear = function () {
@@ -84,5 +120,16 @@
         }
         
         return this;
+    };
+    
+    Store.prototype.addEvent = function (selector, callback) {
+        if (!this.bindings[selector]) { this.bindings[selector] = []; }
+        this.bindings[selector].push(callback);
+    };
+    
+    Store.prototype.removeEvent = function (selector, callback) {
+        for (var i = (this.bindings[selector].length - 1); i >= 0; i--) {
+            if (this.bindings[selector][i] === callback) { this.bindings[selector].splice(i, 1); }
+        }
     };
 }());
